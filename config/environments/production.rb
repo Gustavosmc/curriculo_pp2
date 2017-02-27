8bb851e5c107f43860dfe165c3301cb097266e6b
Luiz Gustavo Santos <luizgustavosmc@gmail.com>
2017-02-27 00:45:10 +0000

    heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,18 @@
 Rails.application.configure do
-  # Settings specified here will take precedence over those in config/application.rb.
+ 
+  config.action_mailer.raise_delivery_errors = true
+  # Configurado para enviar email do Cloud9
+  config.action_mailer.default_url_options = { host: 'https://limitless-temple-43644.herokuapp.com' }
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               env["GMAIL_DOMAIN"],
+    user_name:            env["GMAIL_USERNAME"],
+    password:             env["GMAIL_PASSWORD"],
+    authentication:       'plain',
+    enable_starttls_auto: true  }
 
   # Code is not reloaded between requests.
   config.cache_classes = true
