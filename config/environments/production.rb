7545f30529b098d6616d9c5ff744f88a4c60c32f
Luiz Gustavo Santos <luizgustavosmc@gmail.com>
2017-03-29 23:10:32 +0000

    ...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,6 +19,7 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
   
+  
   config.serve_static_files = true
 
   # Compress JavaScripts and CSS.
@@ -27,7 +28,7 @@ Rails.application.configure do
 
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
 
   
